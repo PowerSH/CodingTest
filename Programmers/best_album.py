6d185f3924e436492b45afeaa7c35cf59471b8f2
PowerSH <psh95king@gmail.com>
2022-02-05 14:39:27 +0900

    update

--- Programmers/best_album.py
+++ Programmers/best_album.py
@@ -1,23 +1,31 @@
+import collections
+# https://bomoto.tistory.com/75
 def solution(genres, plays):
-    hash = {}
-    counter = 0
-
-    for idx in genres:
-        if idx in hash:
-            hash[idx] += plays[counter]
-        else:
-            hash[idx] = plays[counter]
-
-        counter += 1
-
-    print("Current Hash : ", hash)    
-
-
-
     answer = []
+
+    hash_genre = collections.defaultdict(int)
+    hash_play = collections.defaultdict(list)    
+
+    for idx, genre, play in zip(range(len(genres)), genres, plays):        
+        hash_genre[genre] += play
+        hash_play[genre].append([idx, play])
+
+    sorted_genre = sorted(hash_genre.items(), key=lambda x: x[1], reverse=True)    
+
+    for key, value in sorted_genre:
+        sorted_play = sorted(hash_play[key], key=lambda x: x[1], reverse=True)
+
+        for idx, play in sorted_play[:2]:
+            answer.append(idx)
+
+    print(answer)
+
     return answer
 
 genres = ["classic", "pop", "classic", "classic", "pop"]
 plays = [500, 600, 150, 800, 2500]
+
+genres = ["classic", "pop", "classic", "pop", "classic", "classic"]
+plays = [400, 600, 150, 2500, 500, 500]
 
 solution(genres, plays)
